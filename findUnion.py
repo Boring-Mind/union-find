1e28efa6f9af9bd9ef1e7687df7b0c1e9a303a6f
BoringMind <super.d3280@ya.ru>
2019-10-17 15:29:21 +0300

    Formatting code, according to the PEP standard

--- findUnion.py
+++ findUnion.py
@@ -1,53 +1,61 @@
 class UF:
+    """Contains unions and can add items to union or check connectivity"""
 
     components = []
 
-    def union(self, p, q):
-        qFound = False
-        pFound = False
+    def union(self, p: int, q: int):
+        """Creates union between p and q. Creates p or q, if necessary"""
+
+        q_found = False
+        p_found = False
         # print(p, q)
 
-        if (self.connected(p, q)):
+        if self.connected(p, q):
             # print("p and q are connected")
             return
 
         for layer in self.components:
             if q in layer:
-                qLayer = layer
-                qFound = True
+                q_layer = layer
+                q_found = True
                 # print("q Finded")
 
         for layer in self.components:
             if p in layer:
-                pFound = True
-                if qFound == True:
-                    self.components[self.components.index(layer)] = layer.union(qLayer)
-                    self.components.remove(qLayer)
+                p_found = True
+                if q_found is True:
+                    self.components[self.components.index(layer)] = layer.union(q_layer)
+                    self.components.remove(q_layer)
 
-                    # layer.union(qLayer)
+                    # layer.union(q_layer)
                     # print("p and q finded", layer)
                 else:
                     layer.add(q)
                     # print("p finded", layer)
-        
-        if pFound == False:
-            if qFound == True:
-                qLayer.add(p)
+        if p_found is False:
+            if q_found is True:
+                q_layer.add(p)
             else:
                 self.components.append(set((p, q)))
                 # print("none is finded", set((p, q)))
 
-    def getList(self):
-        return self.components
+    def print_list(self):
+        """Prints components list without formatting"""
 
+        print(self.components)
 
-    def connected (self, p, q):
+    def connected(self, p: int, q: int) -> bool:
+        """Checks, whether p and q are in one union or not"""
+
         for layer in self.components:
             if (p in layer) & (q in layer):
                 return True
         return False
 
-def inputFromFile(filename):
+
+def input_from_file(filename: str) -> [str]:
+    """Reads given file and initialize components list"""
+
     file_object = open(filename, "r")
     lines = str(file_object.read())
     file_object.close()
@@ -55,8 +63,11 @@ def inputFromFile(filename):
     lines = lines.split('\n')
     return [line.split(' ') for line in lines[1:]]
 
+
 def main():
-    lines = inputFromFile("input.txt")
+    """Initializes program and run it"""
+
+    lines = input_from_file("input.txt")
 
     print(lines)
 
@@ -79,7 +90,8 @@ def main():
     # uf.union(10421, 16)
     # print(uf.getList())
 
-    print(uf.getList())
+    uf.print_list()
+
 
 if __name__ == "__main__":
     main()
